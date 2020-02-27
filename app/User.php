d41b4b713162060976f351c9658c00d6e1930179
exxxar <exxxar@gmail.com>
2020-02-27 14:19:47 +0300

    обновил поля юзеров

--- app/User.php
+++ app/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'active', 'activation_token'
+        'name', 'email', 'password', 'active', 'activation_token','address','mobile_number'
     ];
 
     /**
